669b6302ea863b814454ef46e0b06317459fcab5
SpiceWeasel66 <nagengasteva@gmail.com>
2023-08-20 20:46:21 +0100

    change display of playlist

--- java.js
+++ java.js
@@ -1,10 +1,18 @@
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
- 
   const element = document.getElementById("songTwo");
   const options = {
+    width: `40%`,
+    height: `200`,
     uri: "spotify:track:1dEOKKLDpgHJGYKmMujxrN",
   };
-  const callback = (EmbedController) => {};
+  const callback = (EmbedController) => {
+    document.querySelectorAll(".episode").forEach((episode) => {
+      episode.addEventListener("click", () => {
+        EmbedController.loadUri(episode.dataset.spotifyId);
+      });
+    });
+  };
+
   IFrameAPI.createController(element, options, callback);
 };
  
